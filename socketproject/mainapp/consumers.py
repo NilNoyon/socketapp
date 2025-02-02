d038d582f1bc45507fe532dd909f6415aa5de999
noyon-sqr <noyon@esquire.com.bd>
2025-02-03 03:07:13 +0600

    noyon : practicing websocket

---
+++ socketproject/mainapp/consumers.py
@@ -0,0 +1,29 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class MyConsumer(WebsocketConsumer):
+     def connect(self):
+          self.accept()
+          self.send(text_data=json.dumps({
+               'message': 'GeeksforGeeks'
+          }))
+
+     def disconnect(self, close_code):
+          pass
+
+     def receive(self, text_data):
+          pass
+    
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+     async def connect(self):
+          await self.accept()
+     async def disconnect(self, close_code):
+          pass
+     async def receive(self, text_data):
+          text_data_json = json.loads(text_data)
+          message = text_data_json['message']
+          await self.send(text_data=json.dumps({
+               'message': message
+          }))
